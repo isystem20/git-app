86b31578671043dca2c0b1e8c01ef739c6c0a657
John <isystem20@gmail.com>
2021-07-19 00:50:45 +0800

    tried guzzle for concurrency request

--- kumu-git-app/app/Http/Controllers/UserController.php
+++ kumu-git-app/app/Http/Controllers/UserController.php
@@ -2,19 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Client\Pool;
 use App\User;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Promise;
+use GuzzleHttp\Psr7\Request;
+
 class UserController extends Controller
 {
 
-    protected $github_url;
+    protected $github_uri;
     
     public function __construct() {
-        $this->github_url = 'https://api.github.com';
+        $this->github_uri = 'https://api.github.com/users';
     }
 
     /**
@@ -26,58 +30,94 @@ class UserController extends Controller
     {
         $users = DB::table('users')->orderby('name','desc')->limit(10)->get();
 
-        foreach ($users as $value) {
-            $github_user = $this->retrieveGithubRecords($value);
-            $value->github_data = $github_user;
-        }
-        return $users;
+        // foreach ($users as $value) {
+        //     $github_user = $this->retrieveGithubRecords($value);
+        //     $value->github_data = $github_user;
+        // }
+        // return $users;
 
+        return $this->fetchGithubData();
     }
 
 
-    private function retrieveGithubRecords($user) {
+    private function fetchGithubData() {
+        $client = new Client(['base_uri' => $this->github_uri]);
+        // Initiate each request but do not block
+        $promises = [
+            0 => $client->getAsync('/isystem20'),
+        ];
 
-        // $responses = Http::pool(fn (Pool $pool) =>
-        // [
-        //     foreach ($users as $value) {
-        //         $pool->as('first')->get('http://localhost/first'),
-        //     } 
+        $headers = ['accept' => 'application/vnd.github.v3+json'];
 
-        // ]);
 
-        // $fn2 = function (\Illuminate\Http\Client\Pool $pool) use ($a) {
-        //     foreach ($users as $value) {
-        //         $arrayPools[] = $pool->get($aVal);
+        $request = new Request('GET', $this->github_uri . '/isystem20', $headers, null);
+        $promise = $client->send($request);
+        // $promise->then(
+        //     function (ResponseInterface $res) {
+        //         echo $promise->getBody();
+        //         echo $res->getStatusCode() . "123\n123";
+        //     },
+        //     function (RequestException $e) {
+        //         echo $promise->getBody();
+        //         echo $e->getMessage() . "111\n2222";
+        //         // echo $e->getRequest()->getMethod();
         //     }
-        //     return $arrayPools;
-        // };
+        // );
 
-        // foreach ($users as $value) {
+        return $promise->getBody();
 
-        // $github_record = Http::withHeaders([
-        //     'Accept' => 'application/vnd.github.v3+json'
-        // ])->get($this->github_url . '/users' . '/' . $user->github_username);
+
+        // Wait for the requests to complete; throws a ConnectException
+        // if any of the requests fail
+        // $responses = Promise\Utils::unwrap($promises);
+    }
+
+
+
+
+    // private function retrieveGithubRecords($user) {
+
+    //     // $responses = Http::pool(fn (Pool $pool) =>
+    //     // [
+    //     //     foreach ($users as $value) {
+    //     //         $pool->as('first')->get('http://localhost/first'),
+    //     //     } 
+
+    //     // ]);
+
+    //     // $fn2 = function (\Illuminate\Http\Client\Pool $pool) use ($a) {
+    //     //     foreach ($users as $value) {
+    //     //         $arrayPools[] = $pool->get($aVal);
+    //     //     }
+    //     //     return $arrayPools;
+    //     // };
+
+    //     // foreach ($users as $value) {
+
+    //     // $github_record = Http::withHeaders([
+    //     //     'Accept' => 'application/vnd.github.v3+json'
+    //     // ])->get($this->github_url . '/users' . '/' . $user->github_username);
         
 
-        $github_record = Http::withHeaders([
-            'Accept' => 'application/vnd.github.v3+json'
-        ])::pool(fn (Pool $pool) => [
-            $pool->get($this->github_url . '/users' . '/' . $user->github_username),
-        ]);
+    //     $github_record = Http::withHeaders([
+    //         'Accept' => 'application/vnd.github.v3+json'
+    //     ])::pool(fn (Pool $pool) => [
+    //         $pool->get($this->github_url . '/users' . '/' . $user->github_username),
+    //     ]);
 
 
-        if ($github_record[0]->status() == 404) {
-            $response = [
-                'message' => 'Github account not found'
-            ];
-        }
-        else {
-            $response = $github_record[0]->json();
-        }
+    //     if ($github_record[0]->status() == 404) {
+    //         $response = [
+    //             'message' => 'Github account not found'
+    //         ];
+    //     }
+    //     else {
+    //         $response = $github_record[0]->json();
+    //     }
 
-        return $response;
+    //     return $response;
 
-    }
+    // }
 
 
 
